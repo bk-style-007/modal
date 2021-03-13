a3c6a5de41dcf337b7c9e3b409a191350cde3cb3
M Bilal Asgher <64664050+bk-style-007@users.noreply.github.com>
2021-03-13 09:26:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const openModalBtn = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < openModalBtn.length; i++)
+  openModalBtn[i].addEventListener('click', openModal);
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
